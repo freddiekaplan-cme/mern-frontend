99c4d5964bcec9ab4475fa15a2e9d4f364b92c66
Freddie CME <freddie.kaplan@edu.cmeducations.se>
2023-12-12 08:24:03 +0100

    Add header and header style.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,18 @@
-import "./Header.module.css";
+import { Link } from 'react-router-dom';
+import classes from './Header.module.css';
 
 const Header = () => (
-  <div>
-    <h1>Changedit</h1>
-    <main>
-      <div>Register</div>
-      <div>Log in</div>
-      <div>Profile</div>
-    </main>
-  </div>
-);
+    <div className={classes.header}>
+        <h1>Changedit</h1>
+        <div className={classes.headerActions}>
+            <Link to="/sign-up">
+                <button>Sign up</button>
+            </Link>
+            <Link to="/sign-in">
+                <button>Sign in</button>
+            </Link>
+        </div>
+    </div>
+)
 
 export default Header;
